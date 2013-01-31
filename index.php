bc20bf8db5ce2294af59699155a2b8c5e87908b7
dev.team.loonghymn.com <dev.team@loonghymn.com>
2013-01-31 21:50:50 +0800

    init

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title></title>
+        <link href="../resources/bootstrap/css/bootstrap.css" type="text/css" rel="stylesheet"/>
+        <link href="../resources/bootstrap/css/bootstrap-responsive.css" type="text/css" rel="stylesheet"/>
+        <link href="http://www.bootcss.com/assets/css/docs.css" type="text/css" rel="stylesheet"/>
+        <link href="http://www.bootcss.com/assets/css/docs-cn.css" type="text/css" rel="stylesheet"/>
+        <link href="http://www.bootcss.com/assets/js/google-code-prettify/prettify.css" type="text/css" rel="stylesheet"/>
+        <script src="../resources/js/jquery.js" type="text/javascript"></script>  
+        <script src="../resources/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+        <script src="http://www.bootcss.com/assets/js/holder/holder.js" type="text/javascript"></script> 
+        <script src="http://www.bootcss.com/assets/js/google-code-prettify/prettify.js" type="text/javascript"></script> 
+        <script src="http://www.bootcss.com/assets/js/application.js" type="text/javascript"></script> 
+
+        <script src="../resources/js/form2json.js" type="text/javascript"></script>
+        <script src="../resources/js/jsrender.js" type="text/javascript"></script>
+        <script src="../resources/js/jquery.observable.js" type="text/javascript"></script>
+        <script src="../resources/js/jquery.views.js" type="text/javascript"></script> 
+        <script src="http://localhost/webui/resources/amplify/amplify.js"></script> 
+    </head>
+    <body>
+        <?php
+        // put your code here
+        //phpinfo();
+        ?>
+    </body>
+</html>
